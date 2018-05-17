5a1b7cbb46340171078e3778e0164d124b084132
AmenderaLochan <32901705+Shalmosis@users.noreply.github.com>
2018-05-17 23:53:16 +0200

    Add files via upload
    Feature : Inscription -> redirige vers la page nouveau_profil
    Feature : Nouveau Profil -> Affichage des champs "date de naissance, description, sports pratiqués et niveaux" SANS VALIDATION EN BASE.

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+    session_start();
+
+    $db_host = "localhost";
+    $db_login = "root";
+    $db_password = "";
+    $db_name = "jc_glm_m2l";
+    
+    $connect = mysqli_connect ($db_host, $db_login, $db_password, $db_name);
+
+    include ('fonctions.php');
+
+
+   
+
+    
+
+
+
+?>
